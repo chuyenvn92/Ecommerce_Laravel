43fc1d3709fe77a49be680c51c6a75b688b93bba
chuyen <maicongchuyentb@gmail.com>
2020-08-23 11:16:20 +0700

    add route category + cắt giao diện phần category.blade.php

--- app/Http/Controllers/Admin/Category/CategoryController.php
+++ app/Http/Controllers/Admin/Category/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin\Category;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Model\Admin\Category;
 
 class CategoryController extends Controller
 {
@@ -12,5 +13,9 @@ class CategoryController extends Controller
         $this->middleware('auth:admin');
     }
 
-    
+    public function category()
+    {
+        $category = Category::all();
+        return view('admin.category.category', compact('category'));
+    }
 }
