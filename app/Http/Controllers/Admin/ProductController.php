1bcb27e38cd4832bbb9a983881d3098d2ac6a437
chuyen <maicongchuyentb@gmail.com>
2020-08-26 09:13:37 +0700

    hiển thị sản phẩm lên All product

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -15,7 +15,15 @@ class ProductController extends Controller
     }
 
     public function index()
-    { }
+    {
+        $product = DB::table('products')
+            ->join('categories', 'products.category_id', 'categories.id')
+            ->join('brands', 'products.brand_id', 'brands.id')
+            ->select('products.*', 'categories.category_name', 'brands.brand_name')
+            ->get();
+        // return response()->json($product);
+        return view('admin.product.index', compact('product'));
+    }
 
     public function create()
     {
