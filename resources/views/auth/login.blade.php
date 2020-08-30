f5ff70e28eea653ce12e78b438b2142a8e08cadd
chuyen <maicongchuyentb@gmail.com>
2020-08-30 22:20:49 +0700

    custom giao diện login bên FE

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,55 +1,66 @@
 @extends('layouts.app')
 
 @section('content')
-        <div class="wrapper without_header_sidebar">
-            <!-- contnet wrapper -->
-            <div class="content_wrapper">
-                    <!-- page content -->
-                    <div class="login_page center_container">
-                        <div class="center_content">
-                            <div class="logo">
-                                <img src="{{asset('public/panel/assets/images/logo.png')}}" alt="" class="img-fluid">
-                            </div>
-                            <form action="{{route('login')}}" class="d-block" method="post">
-                                @csrf
-                                <div class="form-group icon_parent">
-                                    <label for="email">E-mail</label>
-                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Email Address">
-                                    <span class="icon_soon_bottom_right"><i class="fas fa-envelope"></i></span>
-                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                 @enderror
-                                </div>
-                                <div class="form-group icon_parent">
-                                    <label for="password">Password</label>
-                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Password">
-                                        @error('password')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
-                                    <span class="icon_soon_bottom_right"><i class="fas fa-unlock"></i></span>
-                                </div>
-                                <div class="form-group">
-                                    <label class="chech_container">Remember me
-                                        <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-                                        <span class="checkmark"></span>
-                                    </label>
-                                </div>
-                                <div class="form-group">
-                                    <a class="registration" href="{{route('register')}}">Create new account</a><br>
-                                    <a href="{{ route('password.request') }}" class="text-white">I forgot my password</a>
-                                    <button type="submit" class="btn btn-blue">Login</button>
-                                </div>
-                            </form>
-                            <div class="footer">
-                               <p>Copyright &copy; 2020 <a href="https://easylearningbd.com/">easy Learning</a>. All rights reserved.</p>
-                            </div>
-                            
+<link rel="stylesheet" type="text/css" href="{{ asset('public/frontend/styles/contact_styles.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('public/frontend/styles/contact_responsive.css') }}">
+<div class="contact_form">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-5 offset-lg-1" style="border: 1px solid grey; padding:20px; border-radius: 20px;">
+                <div class="contact_form_container">
+                    <div class="contact_form_title text-center">Đăng nhập</div>
+
+                    <form action="#" id="contact_form">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Email hoặc Số điện thoại</label>
+                            <input type="text" class="form-control" aria-describedby="emailHelp" name="email" required="">
                         </div>
-                    </div>
-            </div><!--/ content wrapper -->
-        </div><!--/ wrapper -->
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Mật khẩu</label>
+                            <input type="password" class="form-control" aria-describedby="emailHelp" name="password" required="">
+                        </div>
+                        <div class="contact_form_button">
+                            <button type="submit" class="btn btn-info">Đăng nhập</button>
+                        </div>
+                    </form>
+                    <br>
+                    <button type=submit class="btn btn-primary btn-block"><i class="fab fa-facebook mr-2"></i>Đăng nhập với Facebook</button>
+                    <button type=submit class="btn btn-danger btn-block"><i class="fab fa-google mr-2"></i>Đăng nhập với Google</button>
+                </div>
+            </div>
+            <div class="col-lg-5 offset-lg-1" style="border: 1px solid grey; padding:20px; border-radius: 20px;">
+                <div class="contact_form_container">
+                    <div class="contact_form_title text-center">Đăng kí</div>
+
+                    <form action="#" id="contact_form">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Tên của bạn</label>
+                            <input type="text" class="form-control" aria-describedby="emailHelp" placeholder="tên của bạn" name="name" required="">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Số điện thoại</label>
+                            <input type="text" class="form-control" aria-describedby="emailHelp" placeholder="Nhập phone" name="phone" required="">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Email</label>
+                            <input type="email" class="form-control" aria-describedby="emailHelp" placeholder="Email" name="email" required="">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Mật khẩu</label>
+                            <input type="password" class="form-control" aria-describedby="emailHelp" placeholder="Nhập mật khẩu" name="password" required="">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Nhập lại mật khẩu</label>
+                            <input type="password" class="form-control" aria-describedby="emailHelp" placeholder="Nhập lại mật khẩu" name="password_confirmation" required="">
+                        </div>
+                        <div class="contact_form_button">
+                            <button type="submit" class="btn btn-info">Đăng kí</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="panel"></div>
+</div>
 @endsection
