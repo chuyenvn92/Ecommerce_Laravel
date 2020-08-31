2666d46fe12022b9f235e428f9730d203cafeba7
chuyen <maicongchuyentb@gmail.com>
2020-08-31 15:54:34 +0700

    custome giao diện sau khi ng dùng đăng nhập, thêm cột avatar trong table users

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,76 +4,75 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Auth;
+
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+  /**
+   * Create a new controller instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    $this->middleware('auth');
+  }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-    public function index()
-    {
-        return view('home');
-    }
+  /**
+   * Show the application dashboard.
+   *
+   * @return \Illuminate\Contracts\Support\Renderable
+   */
+  public function index()
+  {
+    return view('home');
+  }
 
-    public function changePassword(){
-        return view('auth.changepassword');
-    }
+  public function changePassword()
+  {
+    return view('auth.changepassword');
+  }
 
-    public function updatePassword(Request $request)
-    {
-      $password=Auth::user()->password;
-      $oldpass=$request->oldpass;
-      $newpass=$request->password;
-      $confirm=$request->password_confirmation;
-      if (Hash::check($oldpass,$password)) {
-           if ($newpass === $confirm) {
-                      $user=User::find(Auth::id());
-                      $user->password=Hash::make($request->password);
-                      $user->save();
-                      Auth::logout();  
-                      $notification=array(
-                        'messege'=>'Password Changed Successfully ! Now Login with Your New Password',
-                        'alert-type'=>'success'
-                         );
-                       return Redirect()->route('login')->with($notification); 
-                 }else{
-                     $notification=array(
-                        'messege'=>'New password and Confirm Password not matched!',
-                        'alert-type'=>'error'
-                         );
-                       return Redirect()->back()->with($notification);
-                 }     
-      }else{
-        $notification=array(
-                'messege'=>'Old Password not matched!',
-                'alert-type'=>'error'
-                 );
-               return Redirect()->back()->with($notification);
+  public function updatePassword(Request $request)
+  {
+    $password = Auth::user()->password;
+    $oldpass = $request->oldpass;
+    $newpass = $request->password;
+    $confirm = $request->password_confirmation;
+    if (Hash::check($oldpass, $password)) {
+      if ($newpass === $confirm) {
+        $user = User::find(Auth::id());
+        $user->password = Hash::make($request->password);
+        $user->save();
+        Auth::logout();
+        $notification = array(
+          'messege' => 'Thay đổi Mật khẩu thành công. Bạn có thể đăng nhập',
+          'alert-type' => 'success'
+        );
+        return Redirect()->route('login')->with($notification);
+      } else {
+        $notification = array(
+          'messege' => 'Mật khẩu không khớp',
+          'alert-type' => 'error'
+        );
+        return Redirect()->back()->with($notification);
       }
-
+    } else {
+      $notification = array(
+        'messege' => 'Mật khẩu cũ không chính xác',
+        'alert-type' => 'error'
+      );
+      return Redirect()->back()->with($notification);
     }
+  }
 
-    public function Logout()
-    {
-        // $logout= Auth::logout();
-            Auth::logout();
-            $notification=array(
-                'messege'=>'Successfully Logout',
-                'alert-type'=>'success'
-                 );
-             return Redirect()->route('login')->with($notification);
-       
-
-    }
+  public function Logout()
+  {
+    // $logout= Auth::logout();
+    Auth::logout();
+    $notification = array(
+      'messege' => 'Đăng xuất thành công',
+      'alert-type' => 'success'
+    );
+    return Redirect()->route('login')->with($notification);
+  }
 }
