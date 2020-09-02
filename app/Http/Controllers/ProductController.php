f8493f508d442e54f19a78cd30b808c4b0dcf0ea
maicongchuyen <maicongchuyentb@gmail.com>
2020-09-02 08:20:37 +0700

    custom details product + add helper laravel => limit str

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -13,8 +13,8 @@ class ProductController extends Controller
             ->join('categories', 'products.category_id', 'categories.id')
             ->join('subcategories', 'products.subcategory_id', 'subcategories.id')
             ->join('brands', 'products.brand_id', 'brands.id')
-            ->select('product.*', 'categories.category_name', 'subcategories.subcategory_name', 'brands.brand_name')
-            ->where('product_id', $id)->first();
+            ->select('products.*', 'categories.category_name', 'subcategories.subcategory_name', 'brands.brand_name')
+            ->where('products.id', $id)->first();
         $color = $product->product_color;
         $product_color = explode(',', $color);
 
--- resources/views/pages/product_details.blade.php
+++ resources/views/pages/product_details.blade.php
@@ -12,25 +12,27 @@
    <!-- Images -->
    <div class="col-lg-2 order-lg-1 order-2">
     <ul class="image_list">
-     <li data-image="{{ asset('public/frontend/images/single_4.jpg') }}"><img src="{{ asset('public/frontend/images/single_4.jpg') }}" alt=""></li>
-     <li data-image="{{ asset('public/frontend/images/single_2.jpg') }}"><img src="{{ asset('public/frontend/images/single_2.jpg') }}" alt=""></li>
-     <li data-image="{{ asset('public/frontend/images/single_3.jpg') }}"><img src="{{ asset('public/frontend/images/single_3.jpg') }}" alt=""></li>
+     <li data-image="{{ asset($product->image_one) }}"><img src="{{ asset($product->image_one) }}" alt=""></li>
+     <li data-image="{{ asset($product->image_two) }}"><img src="{{ asset($product->image_two) }}" alt=""></li>
+     <li data-image="{{ asset($product->image_three) }}"><img src="{{ asset($product->image_three) }}" alt=""></li>
     </ul>
    </div>
 
    <!-- Selected Image -->
    <div class="col-lg-5 order-lg-2 order-1">
-    <div class="image_selected"><img src="{{ asset('public/frontend/images/single_4.jpg') }}" alt=""></div>
+    <div class="image_selected"><img src="{{ asset($product->image_one) }}" alt=""></div>
    </div>
 
    <!-- Description -->
    <div class="col-lg-5 order-3">
     <div class="product_description">
-     <div class="product_category">Laptops</div>
-     <div class="product_name">MacBook Air 13</div>
+     <div class="product_category">{{ $product->category_name }} -> {{ $product->subcategory_name }}</div>
+     <div class="product_name">{{ $product->product_name }}</div>
      <div class="rating_r rating_r_4 product_rating"><i></i><i></i><i></i><i></i><i></i></div>
      <div class="product_text">
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas fermentum. laoreet turpis, nec sollicitudin dolor cursus at. Maecenas aliquet, dolor a faucibus efficitur, nisi tellus cursus urna, eget dictum lacus turpis.</p>
+      <p>
+       {!! str_limit($product->product_details, $limit = 1200) !!}
+      </p>
      </div>
      <div class="order_info d-flex flex-row">
       <form action="#">
@@ -38,7 +40,7 @@
 
         <!-- Product Quantity -->
         <div class="product_quantity clearfix">
-         <span>Quantity: </span>
+         <span>Số lượng: </span>
          <input id="quantity_input" type="text" pattern="[0-9]*" value="1">
          <div class="quantity_buttons">
           <div id="quantity_inc_button" class="quantity_inc quantity_control"><i class="fas fa-chevron-up"></i></div>
@@ -70,10 +72,13 @@
         </ul>
 
        </div>
-
-       <div class="product_price">$2000</div>
+       @if($product->discount_price == null)
+       <div class="product_price">{{ $product->selling_price }}đ</div>
+       @else
+       <div class="product_price">{{ $product->discount_price }}đ<span>{{ $product->selling_price }}đ</span></div>
+       @endif
        <div class="button_container">
-        <button type="button" class="button cart_button">Add to Cart</button>
+        <button type="button" class="button cart_button">Mua ngay</button>
         <div class="product_fav"><i class="fas fa-heart"></i></div>
        </div>
 
@@ -93,110 +98,28 @@
   <div class="row">
    <div class="col">
     <div class="viewed_title_container">
-     <h3 class="viewed_title">Recently Viewed</h3>
+     <h3 class="viewed_title">Thông tin sản phẩm</h3>
      <div class="viewed_nav_container">
       <div class="viewed_nav viewed_prev"><i class="fas fa-chevron-left"></i></div>
       <div class="viewed_nav viewed_next"><i class="fas fa-chevron-right"></i></div>
      </div>
     </div>
 
-    <div class="viewed_slider_container">
-
-     <!-- Recently Viewed Slider -->
-
-     <div class="owl-carousel owl-theme viewed_slider">
-
-      <!-- Recently Viewed Item -->
-      <div class="owl-item">
-       <div class="viewed_item discount d-flex flex-column align-items-center justify-content-center text-center">
-        <div class="viewed_image"><img src="images/view_1.jpg" alt=""></div>
-        <div class="viewed_content text-center">
-         <div class="viewed_price">$225<span>$300</span></div>
-         <div class="viewed_name"><a href="#">Beoplay H7</a></div>
-        </div>
-        <ul class="item_marks">
-         <li class="item_mark item_discount">-25%</li>
-         <li class="item_mark item_new">new</li>
-        </ul>
-       </div>
-      </div>
-
-      <!-- Recently Viewed Item -->
-      <div class="owl-item">
-       <div class="viewed_item d-flex flex-column align-items-center justify-content-center text-center">
-        <div class="viewed_image"><img src="images/view_2.jpg" alt=""></div>
-        <div class="viewed_content text-center">
-         <div class="viewed_price">$379</div>
-         <div class="viewed_name"><a href="#">LUNA Smartphone</a></div>
-        </div>
-        <ul class="item_marks">
-         <li class="item_mark item_discount">-25%</li>
-         <li class="item_mark item_new">new</li>
-        </ul>
-       </div>
-      </div>
-
-      <!-- Recently Viewed Item -->
-      <div class="owl-item">
-       <div class="viewed_item d-flex flex-column align-items-center justify-content-center text-center">
-        <div class="viewed_image"><img src="images/view_3.jpg" alt=""></div>
-        <div class="viewed_content text-center">
-         <div class="viewed_price">$225</div>
-         <div class="viewed_name"><a href="#">Samsung J730F...</a></div>
-        </div>
-        <ul class="item_marks">
-         <li class="item_mark item_discount">-25%</li>
-         <li class="item_mark item_new">new</li>
-        </ul>
-       </div>
-      </div>
-
-      <!-- Recently Viewed Item -->
-      <div class="owl-item">
-       <div class="viewed_item is_new d-flex flex-column align-items-center justify-content-center text-center">
-        <div class="viewed_image"><img src="images/view_4.jpg" alt=""></div>
-        <div class="viewed_content text-center">
-         <div class="viewed_price">$379</div>
-         <div class="viewed_name"><a href="#">Huawei MediaPad...</a></div>
-        </div>
-        <ul class="item_marks">
-         <li class="item_mark item_discount">-25%</li>
-         <li class="item_mark item_new">new</li>
-        </ul>
-       </div>
-      </div>
-
-      <!-- Recently Viewed Item -->
-      <div class="owl-item">
-       <div class="viewed_item discount d-flex flex-column align-items-center justify-content-center text-center">
-        <div class="viewed_image"><img src="images/view_5.jpg" alt=""></div>
-        <div class="viewed_content text-center">
-         <div class="viewed_price">$225<span>$300</span></div>
-         <div class="viewed_name"><a href="#">Sony PS4 Slim</a></div>
-        </div>
-        <ul class="item_marks">
-         <li class="item_mark item_discount">-25%</li>
-         <li class="item_mark item_new">new</li>
-        </ul>
-       </div>
-      </div>
-
-      <!-- Recently Viewed Item -->
-      <div class="owl-item">
-       <div class="viewed_item d-flex flex-column align-items-center justify-content-center text-center">
-        <div class="viewed_image"><img src="images/view_6.jpg" alt=""></div>
-        <div class="viewed_content text-center">
-         <div class="viewed_price">$375</div>
-         <div class="viewed_name"><a href="#">Speedlink...</a></div>
-        </div>
-        <ul class="item_marks">
-         <li class="item_mark item_discount">-25%</li>
-         <li class="item_mark item_new">new</li>
-        </ul>
-       </div>
-      </div>
-     </div>
-
+    <ul class="nav nav-tabs" id="myTab" role="tablist">
+     <li class="nav-item" role="presentation">
+      <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Chi tiết</a>
+     </li>
+     <li class="nav-item" role="presentation">
+      <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Video</a>
+     </li>
+     <li class="nav-item" role="presentation">
+      <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Đánh giá sản phẩm</a>
+     </li>
+    </ul>
+    <div class="tab-content" id="myTabContent">
+     <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab"><br>{!! $product->product_details !!}</div>
+     <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab"><br>{{ $product->video_link}}</div>
+     <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab"><br>hí</div>
     </div>
    </div>
   </div>
