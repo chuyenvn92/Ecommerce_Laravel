0d24a6f7420c9df6b76a8e1227c386857ad651f0
maicongchuyen <maicongchuyentb@gmail.com>
2020-09-15 08:00:05 +0700

    sigment header menu part to every pasge

--- resources/views/pages/blog.blade.php
+++ resources/views/pages/blog.blade.php
@@ -1,40 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
+@include('layouts.menubar')
 <link rel="stylesheet" type="text/css" href="{{ asset('public/frontend/styles/blog_styles.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('public/frontend/styles/blog_responsive.css') }}">
 
 
 <div class="blog">
- <div class="container">
-  <div class="row">
-   <div class="col">
-    <div class="blog_posts d-flex flex-row align-items-start justify-content-between">
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <div class="blog_posts d-flex flex-row align-items-start justify-content-between">
 
-     @foreach($post as $row)
-     <!-- Blog post -->
-     <div class="blog_post">
-      <div class="blog_image" style="background-image:url({{ asset($row->post_image) }})"></div>
-      <div class="blog_text">
-       @if(Session()->get('lang') == 'vietnam')
-       {{ $row->post_title_vn}}
-       @else
-       {{ $row->post_title_en}}
-       @endif
+          @foreach($post as $row)
+          <!-- Blog post -->
+          <div class="blog_post">
+            <div class="blog_image" style="background-image:url({{ asset($row->post_image) }})"></div>
+            <div class="blog_text">
+              @if(Session()->get('lang') == 'vietnam')
+              {{ $row->post_title_vn}}
+              @else
+              {{ $row->post_title_en}}
+              @endif
+            </div>
+            <div class="blog_button"><a href="{{ url('blog/single/'.$row->id) }}">
+                @if(Session()->get('lang') == 'vietnam')
+                Xem bài viết
+                @else
+                Continue Reading
+                @endif
+              </a></div>
+            @endforeach
+          </div>
+        </div>
+
       </div>
-      <div class="blog_button"><a href="{{ url('blog/single/'.$row->id) }}">
-        @if(Session()->get('lang') == 'vietnam')
-        Xem bài viết
-        @else
-        Continue Reading
-        @endif
-       </a></div>
-      @endforeach
-     </div>
     </div>
-
-   </div>
   </div>
- </div>
- <script src="{{ asset('public/frontend/js/blog_custom.js') }}"></script>
- @endsection
+  <script src="{{ asset('public/frontend/js/blog_custom.js') }}"></script>
+  @endsection
--- resources/views/pages/blog_single.blade.php
+++ resources/views/pages/blog_single.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+@include('layouts.menubar')
 <link rel="stylesheet" type="text/css" href="{{ asset('public/frontend/styles/blog_single_styles.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('public/frontend/styles/blog_single_responsive.css') }}">
 @foreach($posts as $row)
