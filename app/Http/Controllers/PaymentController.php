e07288addb33ccec5538efa73c5e035b8e437967
maicongchuyen <maicongchuyentb@gmail.com>
2020-09-15 08:32:49 +0700

    setup để tích hợp stripe cho thanh toán online

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -8,7 +8,22 @@ class PaymentController extends Controller
 {
     public function Payment(Request $request)
     {
-        $payment = $request->payment;
-        echo "$payment";
+        $data = array();
+        $data['name'] = $request->name;
+        $data['phone'] = $request->phone;
+        $data['email'] = $request->email;
+        $data['address'] = $request->address;
+        $data['city'] = $request->city;
+        $data['payment'] = $request->payment;
+        // dd($data);
+        if ($request->payment == 'stripe') {
+            return view('pages.payment.stripe', compact('data'));
+        } elseif ($request->payment == 'paypal') {
+            echo "Chức năng đang hoàn thiện";
+        } elseif ($request->payment == 'ideal') {
+            echo "Chức năng đang hoàn thiện";
+        } else {
+            echo "Cash on Delivery (Thanh toán khi nhận hàng)";
+        }
     }
 }
