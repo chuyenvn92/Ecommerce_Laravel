e5078234bdb5144a1f6fe340c80f6450746b3d15
maicongchuyen <maicongchuyentb@gmail.com>
2020-09-21 21:58:47 +0700

    hoàn thành thanh toán với stripe, lỗi nhỏ khi sử dụng coupon vẫn k giảm bên stripe

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
+use DB;
+use Cart;
+use Session;
 
 class PaymentController extends Controller
 {
@@ -28,6 +32,7 @@ class PaymentController extends Controller
   }
   public function stripeCharge(Request $request)
   {
+    $total = $request->total;
     // Set your secret key. Remember to switch to your live secret key in production!
     // See your keys here: https://dashboard.stripe.com/account/apikeys
     \Stripe\Stripe::setApiKey('sk_test_51HRT3nEifoQgfmALCjipnSyJr6zFoLcNUQ4ILjVqKqltPTQ4iR7yGStSaW5K0Mz2gaiLDtKT8ciPNs5ktXFbK9iu00vLNktYKG');
@@ -37,12 +42,68 @@ class PaymentController extends Controller
     $token = $_POST['stripeToken'];
 
     $charge = \Stripe\Charge::create([
-      'amount' => 999 * 100,
-      'currency' => 'usd',
-      'description' => 'Chuyen Test Thanh Toan',
+      'amount' => $total,
+      'currency' => 'vnd',
+      'description' => 'Chuyên Thanh Toán',
       'source' => $token,
       'metadata' => ['order_id' => uniqid()],
     ]);
-    dd($charge);
+    $data = array();
+    $data['user_id'] = Auth::id();
+    $data['payment_id'] = $charge->payment_method;
+    $data['paying_amount'] = $charge->amount;
+    $data['blnc_transection'] = $charge->balance_transaction;
+    $data['stripe_order_id'] = $charge->metadata->order_id;
+    $data['shipping'] = $request->shipping;
+    $data['vat'] = $request->vat;
+    $data['total'] = $request->total;
+    $data['payment_type'] = $request->payment_type;
+
+    if (Session::has('coupon')) {
+      $data['subtotal'] = Session::get('coupon')['balance'];
+    } else {
+      $data['subtotal'] = Cart::Subtotal();
+    }
+    $data['status'] = 0;
+    $data['date'] = date('d-m-y');
+    $data['month'] = date('F');
+    $data['year'] = date('Y');
+    $order_id = DB::table('orders')->insertGetId($data);
+
+    // insert shipping table
+
+    $shipping = array();
+    $shipping['order_id'] = $order_id;
+    $shipping['ship_name'] = $request->ship_name;
+    $shipping['ship_phone'] = $request->ship_phone;
+    $shipping['ship_email'] = $request->ship_email;
+    $shipping['ship_address'] = $request->ship_address;
+    $shipping['ship_city'] = $request->ship_city;
+    DB::table('shipping')->insert($shipping);
+
+    // insert order details table
+    $content = Cart::content();
+    $details = array();
+    foreach ($content as $row) {
+      $details['order_id'] = $order_id;
+      $details['product_id'] = $row->id;
+      $details['product_name'] = $row->name;
+      $details['color'] = $row->options->color;
+      $details['size'] = $row->options->size;
+      $details['quantity'] = $row->qty;
+      $details['singleprice'] = $row->price;
+      $details['totalprice'] = $row->qty * $row->price;
+      DB::table('orders_details')->insert($details);
+    }
+
+    Cart::destroy();
+    if (Session::has('coupon')) {
+      Session::forget('coupon');
+    }
+    $notification = array(
+      'messege' => 'Thanh toán đơn hàng thành công',
+      'alert-type' => 'success'
+    );
+    return Redirect()->to('/')->with($notification);
   }
 }
