da26443f08706bf152faf3cdb881c43e443f9815
maicongchuyen <maicongchuyentb@gmail.com>
2020-10-05 23:43:24 +0700

    update admin dashboard

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,6 +1,23 @@
 @extends('admin.admin_layouts')
 
 @section('admin_content')
+@php
+$date = date('d-m-y');
+$today = DB::table('orders')->where('date', $date)->sum('total');
+
+$month = date('F');
+$month = DB::table('orders')->where('month', $month)->sum('total');
+
+$year = date('Y');
+$year = DB::table('orders')->where('year', $year)->sum('total');
+
+$delevery = DB::table('orders')->where('date', $date)->where('status', 3)->sum('total');
+
+$product = DB::table('products')->get();
+$brand = DB::table('brands')->get();
+$user = DB::table('users')->get();
+@endphp
+
 <!-- ########## START: MAIN PANEL ########## -->
 <div class="sl-mainpanel">
   <div class="sl-pagebody">
@@ -8,26 +25,93 @@
       <div class="col-sm-6 col-xl-3">
         <div class="card pd-20 bg-primary">
           <div class="d-flex justify-content-between align-items-center mg-b-10">
-            <h6 class="tx-11 tx-uppercase mg-b-0 tx-spacing-1 tx-white">Bán trong ngày</h6>
+            <h6 class="tx-11 tx-uppercase mg-b-0 tx-spacing-1 tx-white">Doanh thu trong ngày</h6>
             <a href="" class="tx-white-8 hover-white"><i class="icon ion-android-more-horizontal"></i></a>
           </div><!-- card-header -->
           <div class="d-flex align-items-center justify-content-between">
             <span class="sparkline2">5,3,9,6,5,9,7,3,5,2</span>
-            <h3 class="mg-b-0 tx-white tx-lato tx-bold">850đ</h3>
+            <h3 class="mg-b-0 tx-white tx-lato tx-bold">{{ $today }}đ</h3>
           </div><!-- card-body -->
-          <div class="d-flex align-items-center justify-content-between mg-t-15 bd-t bd-white-2 pd-t-10">
-            <div>
-              <span class="tx-11 tx-white-6">Ví dụ vậy</span>
-              <h6 class="tx-white mg-b-0">2,210đ</h6>
-            </div>
-            <div>
-              <span class="tx-11 tx-white-6">Demo</span>
-              <h6 class="tx-white mg-b-0">320đ</h6>
-            </div>
-          </div><!-- -->
+        </div><!-- card -->
+      </div><!-- col-3 -->
+      <div class="col-sm-6 col-xl-3">
+        <div class="card pd-20 bg-info">
+          <div class="d-flex justify-content-between align-items-center mg-b-10">
+            <h6 class="tx-11 tx-uppercase mg-b-0 tx-spacing-1 tx-white">Doanh thu trong tháng</h6>
+            <a href="" class="tx-white-8 hover-white"><i class="icon ion-android-more-horizontal"></i></a>
+          </div><!-- card-header -->
+          <div class="d-flex align-items-center justify-content-between">
+            <span class="sparkline2">5,3,9,6,5,9,7,3,5,2</span>
+            <h3 class="mg-b-0 tx-white tx-lato tx-bold">{{ $month }}đ</h3>
+          </div><!-- card-body -->
+        </div><!-- card -->
+      </div><!-- col-3 -->
+      <div class="col-sm-6 col-xl-3">
+        <div class="card pd-20 bg-purple">
+          <div class="d-flex justify-content-between align-items-center mg-b-10">
+            <h6 class="tx-11 tx-uppercase mg-b-0 tx-spacing-1 tx-white">Doanh thu trong năm</h6>
+            <a href="" class="tx-white-8 hover-white"><i class="icon ion-android-more-horizontal"></i></a>
+          </div><!-- card-header -->
+          <div class="d-flex align-items-center justify-content-between">
+            <span class="sparkline2">5,3,9,6,5,9,7,3,5,2</span>
+            <h3 class="mg-b-0 tx-white tx-lato tx-bold">{{ $year }}đ</h3>
+          </div><!-- card-body -->
+        </div><!-- card -->
+      </div><!-- col-3 -->
+      <div class="col-sm-6 col-xl-3">
+        <div class="card pd-20 bg-primary">
+          <div class="d-flex justify-content-between align-items-center mg-b-10">
+            <h6 class="tx-11 tx-uppercase mg-b-0 tx-spacing-1 tx-white">Số đơn hàng đã chuyển trong ngày</h6>
+            <a href="" class="tx-white-8 hover-white"><i class="icon ion-android-more-horizontal"></i></a>
+          </div><!-- card-header -->
+          <div class="d-flex align-items-center justify-content-between">
+            <span class="sparkline2">5,3,9,6,5,9,7,3,5,2</span>
+            <h3 class="mg-b-0 tx-white tx-lato tx-bold">{{ $delevery }}đ</h3>
+          </div><!-- card-body -->
+        </div><!-- card -->
+      </div><!-- col-3 -->
+    </div><!-- row -->
+    <br><br>
+
+    <div class="row row-sm">
+      <div class="col-sm-6 col-xl-3">
+        <div class="card pd-20 bg-primary">
+          <div class="d-flex justify-content-between align-items-center mg-b-10">
+            <h6 class="tx-11 tx-uppercase mg-b-0 tx-spacing-1 tx-white">Tổng số sản phẩm</h6>
+            <a href="" class="tx-white-8 hover-white"><i class="icon ion-android-more-horizontal"></i></a>
+          </div><!-- card-header -->
+          <div class="d-flex align-items-center justify-content-between">
+            <span class="sparkline2">5,3,9,6,5,9,7,3,5,2</span>
+            <h3 class="mg-b-0 tx-white tx-lato tx-bold">{{count($product) }}</h3>
+          </div><!-- card-body -->
+        </div><!-- card -->
+      </div><!-- col-3 -->
+      <div class="col-sm-6 col-xl-3">
+        <div class="card pd-20 bg-primary">
+          <div class="d-flex justify-content-between align-items-center mg-b-10">
+            <h6 class="tx-11 tx-uppercase mg-b-0 tx-spacing-1 tx-white">Tổng số thương hiệu</h6>
+            <a href="" class="tx-white-8 hover-white"><i class="icon ion-android-more-horizontal"></i></a>
+          </div><!-- card-header -->
+          <div class="d-flex align-items-center justify-content-between">
+            <span class="sparkline2">5,3,9,6,5,9,7,3,5,2</span>
+            <h3 class="mg-b-0 tx-white tx-lato tx-bold">{{ count($brand) }}</h3>
+          </div><!-- card-body -->
+        </div><!-- card -->
+      </div><!-- col-3 -->
+      <div class="col-sm-6 col-xl-3">
+        <div class="card pd-20 bg-primary">
+          <div class="d-flex justify-content-between align-items-center mg-b-10">
+            <h6 class="tx-11 tx-uppercase mg-b-0 tx-spacing-1 tx-white">Số người dùng</h6>
+            <a href="" class="tx-white-8 hover-white"><i class="icon ion-android-more-horizontal"></i></a>
+          </div><!-- card-header -->
+          <div class="d-flex align-items-center justify-content-between">
+            <span class="sparkline2">5,3,9,6,5,9,7,3,5,2</span>
+            <h3 class="mg-b-0 tx-white tx-lato tx-bold">{{ count($user) }}</h3>
+          </div><!-- card-body -->
         </div><!-- card -->
       </div><!-- col-3 -->
     </div><!-- row -->
   </div><!-- sl-mainpanel -->
-  <!-- ########## END: MAIN PANEL ########## -->
-  @endsection
+</div>
+<!-- ########## END: MAIN PANEL ########## -->
+@endsection
